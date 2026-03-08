5a353f40c5563120f593504620741e122fb1df6d
BeebzTrading <b.ztrading@outlook.com>
2026-03-08 16:24:37 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,157 @@
+const products = [
+  {
+    "name": "Highveld Rose Soap",
+    "price": 59.0,
+    "image": "images/highveld-rose-soap.jpg"
+  },
+  {
+    "name": "Lily of the Vale Soap",
+    "price": 59.0,
+    "image": "images/lily-of-the-vale-soap.jpg"
+  },
+  {
+    "name": "Jacaranda Bloom Soap",
+    "price": 59.0,
+    "image": "images/jacaranda-bloom-soap.jpg"
+  },
+  {
+    "name": "Protea Party Soap",
+    "price": 59.0,
+    "image": "images/protea-party-soap.jpg"
+  },
+  {
+    "name": "Just Beachy Soap",
+    "price": 59.0,
+    "image": "images/just-beachy-soap.jpg"
+  },
+  {
+    "name": "Pacific Waters Soap",
+    "price": 59.0,
+    "image": "images/pacific-waters-soap.jpg"
+  },
+  {
+    "name": "Boulders Cove Soap",
+    "price": 59.0,
+    "image": "images/boulders-cove-soap.jpg"
+  },
+  {
+    "name": "Seashells on the Seaboard Soap",
+    "price": 59.0,
+    "image": "images/seashells-on-the-seaboard-soap.jpg"
+  },
+  {
+    "name": "Port Lizzie Soap",
+    "price": 59.0,
+    "image": "images/port-lizzie-soap.jpg"
+  },
+  {
+    "name": "La Vie in Rose Soap",
+    "price": 59.0,
+    "image": "images/la-vie-in-rose-soap.jpg"
+  },
+  {
+    "name": "Signature Botanical Soap Collection (5 Soap Gift Set)",
+    "price": 249.0,
+    "image": "images/signature-botanical-soap-collection-(5-soap-gift-set).jpg"
+  },
+  {
+    "name": "Floral Collection Bundle",
+    "price": 159.0,
+    "image": "images/floral-collection-bundle.jpg"
+  },
+  {
+    "name": "Coastal Escape Bundle",
+    "price": 159.0,
+    "image": "images/coastal-escape-bundle.jpg"
+  },
+  {
+    "name": "Christmas Cheer Bundle",
+    "price": 179.0,
+    "image": "images/christmas-cheer-bundle.jpg"
+  },
+  {
+    "name": "Easter Joy Bundle",
+    "price": 179.0,
+    "image": "images/easter-joy-bundle.jpg"
+  },
+  {
+    "name": "Birthday Elegance Bundle",
+    "price": 179.0,
+    "image": "images/birthday-elegance-bundle.jpg"
+  },
+  {
+    "name": "Matte Ceramic Soap Dish",
+    "price": 139.0,
+    "image": "images/matte-ceramic-soap-dish.jpg"
+  },
+  {
+    "name": "Waterfall Soap Dish \u2013 Self Draining",
+    "price": 149.0,
+    "image": "images/waterfall-soap-dish-\u2013-self-draining.jpg"
+  },
+  {
+    "name": "Handmade Twine Soap Saver Pouch",
+    "price": 129.0,
+    "image": "images/handmade-twine-soap-saver-pouch.jpg"
+  },
+  {
+    "name": "Highveld Rose Bath Salts",
+    "price": 129.0,
+    "image": "images/highveld-rose-bath-salts.jpg"
+  },
+  {
+    "name": "Lavender Fynbos Bath Salts",
+    "price": 129.0,
+    "image": "images/lavender-fynbos-bath-salts.jpg"
+  },
+  {
+    "name": "Coastal Citrus Bath Salts",
+    "price": 129.0,
+    "image": "images/coastal-citrus-bath-salts.jpg"
+  },
+  {
+    "name": "Rooibos & Honey Bath Salts",
+    "price": 129.0,
+    "image": "images/rooibos-and-honey-bath-salts.jpg"
+  },
+  {
+    "name": "Bloom & Breeze Soak Set",
+    "price": 329.0,
+    "image": "images/bloom-and-breeze-soak-set.jpg"
+  },
+  {
+    "name": "Aromatherapy Shower Steamers",
+    "price": 149.0,
+    "image": "images/aromatherapy-shower-steamers.jpg"
+  },
+  {
+    "name": "Rose & Chamomile Bath Tea",
+    "price": 139.0,
+    "image": "images/rose-and-chamomile-bath-tea.jpg"
+  },
+  {
+    "name": "Fynbos Herbal Bath Tea",
+    "price": 139.0,
+    "image": "images/fynbos-herbal-bath-tea.jpg"
+  },
+  {
+    "name": "Evening Bloom Ritual",
+    "price": 289.0,
+    "image": "images/evening-bloom-ritual.jpg"
+  },
+  {
+    "name": "Botanical Spa Ritual",
+    "price": 399.0,
+    "image": "images/botanical-spa-ritual.jpg"
+  },
+  {
+    "name": "Coastal Refresh Ritual",
+    "price": 399.0,
+    "image": "images/coastal-refresh-ritual.jpg"
+  },
+  {
+    "name": "Complete Botanical Self-Care Ritual Set",
+    "price": 499.0,
+    "image": "images/complete-botanical-self-care-ritual-set.jpg"
+  }
+];
