6daab7c588815c03a24fd09d17b4670c961e380e
BeebzTrading <b.ztrading@outlook.com>
2026-03-08 20:03:14 +0200

    Add files via upload

--- products.js
+++ products.js
@@ -3,16 +3,16 @@ const products = [
 {
 name: "Signature Botanical Soap",
 price: 120,
-image: "images/highveld-rose-box-cat.png"
+image: "images/highveld rose box cat.png"
 },
 {
 name: "Rooibos & Honey Bath Salt",
 price: 150,
-image: "images/rooibos-and-honey-salt-1.png"
+image: "images/rooibos and honey salt 1.png"
 },
 {
 name: "Coastal Citrus Bath Salt",
 price: 150,
-image: "images/coastal-citrus-salt-5.jpeg"
+image: "images/coastal citrus salt 5.jpeg"
 }
 ];
