c190e0872086b64733fb9db8b94ad04efbb1ecc3
BeebzTrading <b.ztrading@outlook.com>
2026-03-08 20:19:39 +0200

    Add files via upload

--- products.js
+++ products.js
@@ -1,18 +1,129 @@
-
 const products = [
+
 {
-name: "Signature Botanical Soap",
-price: 120,
-image: "images/highveld rose box cat.png"
+name:"Highveld Rose Soap",
+price:120,
+image:"images/highveld-rose-box-cat.png"
 },
+
 {
-name: "Rooibos & Honey Bath Salt",
-price: 150,
-image: "images/rooibos and honey salt 1.png"
+name:"Jacaranda Bloom Soap",
+price:120,
+image:"images/jacaranda-bloom-box-cat.png"
 },
+
 {
-name: "Coastal Citrus Bath Salt",
-price: 150,
-image: "images/coastal citrus salt 5.jpeg"
+name:"Lily of the Vale Soap",
+price:120,
+image:"images/lily-of-the-vale-box-cat.png"
+},
+
+{
+name:"Port Lizzie Soap",
+price:120,
+image:"images/port-lizzie-box-cat.png"
+},
+
+{
+name:"Pacific Waters Soap",
+price:120,
+image:"images/pacific-waters-box-cat.png"
+},
+
+{
+name:"Seashells on the Seaboard Soap",
+price:120,
+image:"images/seashells-on-the-seaboard-box-cat.png"
+},
+
+{
+name:"Just Beachy Soap",
+price:120,
+image:"images/just-beachy-box-cat.png"
+},
+
+{
+name:"La Vie in Rose Soap",
+price:120,
+image:"images/la-vie-in-rose-box-cat.png"
+},
+
+{
+name:"Protea Party Soap",
+price:120,
+image:"images/protea-party-box-cat.png"
+},
+
+{
+name:"Boulders Cove Soap",
+price:120,
+image:"images/boulders-cove-box-cat.png"
+},
+
+{
+name:"Highveld Rose Bath Salt",
+price:150,
+image:"images/highveld-rose-salt-2.jpeg"
+},
+
+{
+name:"Lavender Fynbos Bath Salt",
+price:150,
+image:"images/lavender-fynbos-salt-4.jpeg"
+},
+
+{
+name:"Coastal Citrus Bath Salt",
+price:150,
+image:"images/coastal-citrus-salt-5.jpeg"
+},
+
+{
+name:"Rooibos & Honey Bath Salt",
+price:150,
+image:"images/rooibos-and-honey-salt-1.png"
+},
+
+{
+name:"Bloom & Breeze Set",
+price:350,
+image:"images/bloom-and-breeze-set.png"
+},
+
+{
+name:"Coastal Escape Bundle",
+price:350,
+image:"images/coastal-escape-cat.png"
+},
+
+{
+name:"Birthday Bundle",
+price:350,
+image:"images/birthday-bundle-cat.png"
+},
+
+{
+name:"Christmas Bundle",
+price:350,
+image:"images/christmas-bundle-cat.png"
+},
+
+{
+name:"Fynbos Bath Tea",
+price:150,
+image:"images/fynbos-tea-1.png"
+},
+
+{
+name:"Rose Bath Tea",
+price:150,
+image:"images/rose-tea-1.png"
+},
+
+{
+name:"Shower Steamers",
+price:150,
+image:"images/shower-steamers-1.jpg"
 }
+
 ];
