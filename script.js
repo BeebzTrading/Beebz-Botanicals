d009d1bfeb2e8ba4412878d6cc01c9b10bb74a0f
BeebzTrading <b.ztrading@outlook.com>
2026-03-08 15:25:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,79 @@
 
-let cart = JSON.parse(localStorage.getItem("cartData")) || [];
+let cart = JSON.parse(localStorage.getItem("cartData")) || []
 
 function saveCart(){
-localStorage.setItem("cartData",JSON.stringify(cart));
+localStorage.setItem("cartData",JSON.stringify(cart))
 }
 
 function addToCart(name,price,image){
 
-let item=cart.find(p=>p.name===name);
+let item=cart.find(p=>p.name===name)
 
 if(item){
-item.qty++;
+item.qty++
 }else{
-cart.push({name,price,image,qty:1});
+cart.push({name,price,image,qty:1})
 }
 
-saveCart();
-updateCart();
-openCart();
+saveCart()
+updateCart()
+openCart()
 }
 
 function updateCart(){
 
-let items=document.getElementById("cart-items");
-let totalEl=document.getElementById("cart-total");
+let items=document.getElementById("cart-items")
+let totalEl=document.getElementById("cart-total")
 
-if(!items || !totalEl) return;
+if(!items||!totalEl)return
 
-items.innerHTML="";
-let total=0;
+items.innerHTML=""
+let total=0
 
 cart.forEach((item,i)=>{
 
-let div=document.createElement("div");
+let div=document.createElement("div")
 
 div.innerHTML=`
 <img src="${item.image}" class="cart-img">
 ${item.name} x${item.qty}
 <button onclick="removeItem(${i})">Remove</button>
-`;
+`
 
-items.appendChild(div);
+items.appendChild(div)
 
-total+=item.price*item.qty;
+total+=item.price*item.qty
 
-});
+})
 
-totalEl.innerText="Total: R"+total;
-localStorage.setItem("cartTotal",total);
+totalEl.innerText="Total: R"+total
+localStorage.setItem("cartTotal",total)
 }
 
 function removeItem(i){
-cart.splice(i,1);
-saveCart();
-updateCart();
+cart.splice(i,1)
+saveCart()
+updateCart()
 }
-
-
-
-
-
-
-
-function goCheckout(){
-
-let total=localStorage.getItem("cartTotal")||0;
-window.location.href="checkout.html?total="+total;
-
-}
-
-window.onload=function(){
-updateCart();
-};
-
 
 function openCart(){
-const cart=document.getElementById("cart");
-if(cart) cart.classList.add("open");
+const cartEl=document.getElementById("cart")
+if(cartEl)cartEl.classList.add("open")
 }
 
 function closeCart(){
-const cart=document.getElementById("cart");
-if(cart) cart.classList.remove("open");
+const cartEl=document.getElementById("cart")
+if(cartEl)cartEl.classList.remove("open")
 }
 
 function toggleCart(){
-const cart=document.getElementById("cart");
-if(cart) cart.classList.toggle("open");
+const cartEl=document.getElementById("cart")
+if(cartEl)cartEl.classList.toggle("open")
 }
+
+function goCheckout(){
+let total=localStorage.getItem("cartTotal")||0
+window.location.href="checkout.html?total="+total
+}
+
+window.onload=updateCart
