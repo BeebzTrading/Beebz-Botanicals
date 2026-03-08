95baee7269de824b95955606d19fc9d1c1ec029f
BeebzTrading <b.ztrading@outlook.com>
2026-03-08 21:08:10 +0200

    Update products.js

--- products.js
+++ products.js
@@ -2,126 +2,126 @@ const products = [
 
 {
 name:"Highveld Rose Soap",
-price:59,
+price:120,
 image:"images/highveld-rose-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Jacaranda Bloom Soap",
-price:59,
+price:120,
 image:"images/jacaranda-bloom-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Lily of the Vale Soap",
-price:59,
+price:120,
 image:"images/lily-of-the-vale-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Port Lizzie Soap",
-price:59,
+price:120,
 image:"images/port-lizzie-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Pacific Waters Soap",
-price:59,
+price:120,
 image:"images/pacific-waters-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Seashells on the Seaboard Soap",
-price:59,
+price:120,
 image:"images/seashells-on-the-seaboard-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Just Beachy Soap",
-price:59,
+price:120,
 image:"images/just-beachy-box-cat.png",
 category:"soap"
 },
 
 {
 name:"La Vie in Rose Soap",
-price:59,
+price:120,
 image:"images/la-vie-in-rose-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Protea Party Soap",
-price:59,
+price:120,
 image:"images/protea-party-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Boulders Cove Soap",
-price:59,
+price:120,
 image:"images/boulders-cove-box-cat.png",
 category:"soap"
 },
 
 {
 name:"Highveld Rose Bath Salt",
-price:129,
+price:150,
 image:"images/highveld-rose-salt-2.jpeg",
 category:"bath"
 },
 
 {
 name:"Lavender Fynbos Bath Salt",
-price:129,
+price:150,
 image:"images/lavender-fynbos-salt-4.jpeg",
 category:"bath"
 },
 
 {
 name:"Coastal Citrus Bath Salt",
-price:129,
+price:150,
 image:"images/coastal-citrus-salt-5.jpeg",
 category:"bath"
 },
 
 {
 name:"Rooibos & Honey Bath Salt",
-price:129,
+price:150,
 image:"images/rooibos-and-honey-salt-1.png",
 category:"bath"
 },
 
 {
 name:"Fynbos Bath Tea",
-price:139,
+price:150,
 image:"images/fynbos-tea-1.png",
 category:"bath"
 },
 
 {
 name:"Rose Bath Tea",
-price:139,
+price:150,
 image:"images/rose-tea-1.png",
 category:"bath"
 },
 
 {
 name:"Shower Steamers",
-price:149,
+price:150,
 image:"images/shower-steamers-1.jpg",
 category:"bath"
 },
 
 {
 name:"Bloom & Breeze Set",
-price:329,
+price:350,
 image:"images/bloom-and-breeze-set.png",
 category:"bundle"
 },
@@ -135,60 +135,16 @@ category:"bundle"
 
 {
 name:"Birthday Bundle",
-price:179,
+price:350,
 image:"images/birthday-bundle-cat.png",
 category:"bundle"
 },
 
 {
 name:"Christmas Bundle",
-price:179,
+price:350,
 image:"images/christmas-bundle-cat.png",
 category:"bundle"
-},
-
-{
-name:"Easter Bundle",
-price:179,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-{
-name:"Evening Bloom Ritual",
-price:289,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-{
-name:"Botanical Spa Ritual",
-price:399,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-{
-name:"Coastal Refresh Ritual",
-price:399,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-{
-name:"Complete Self-Care Ritual Set",
-price:499,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-{
-name:"Premium Signature Soap Collection",
-price:249,
-image:"images/seashells-on-the-seaboard-box-cat.png",
-category:"bundle"
-},
-
-
+}
 
 ];
