2561dc3cf349ddf5d804833e34f58dbe0ace9959
BeebzTrading <b.ztrading@outlook.com>
2026-03-16 19:19:41 +0200

    Update products.js

--- products.js
+++ products.js
@@ -6,6 +6,7 @@ price:59,
 image:"./images/highveld-rose-box-cat.png",
 category:"soap",
 description:"A romantic floral soap scented with rose, geranium and palmarosa. Made with olive oil, coconut oil and shea butter, it produces a creamy lather that gently cleanses while leaving skin soft and nourished."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -14,6 +15,7 @@ price:59,
 image:"./images/jacaranda-bloom-box-cat.png",
 category:"soap",
  description:"A soothing blend of lavender and mandarin blossom creates a soft floral-citrus fragrance. Made with olive oil, coconut oil and shea butter, this botanical soap produces a gentle, creamy lather that cleanses and leaves skin feeling soft and refreshed."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -22,6 +24,7 @@ price:59,
 image:"./images/lily-of-the-vale-box-cat.png",
 category:"soap",
  description:"An elegant floral blend of white blossoms and powdery petals, softened with vetiver and warm musk for a sophisticated, lingering scent. Made with olive oil, coconut oil, shea butter, jojoba oil and sunflower oil."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -30,6 +33,7 @@ price:59,
 image:"./images/port-lizzie-box-cat.png",
 category:"soap",
  description:"A fresh coastal blend of lemongrass and lime, layered with cedarwood, wood sage and sea salt for a bright ocean-inspired fragrance. Made with olive oil, coconut oil and shea butter."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -38,6 +42,7 @@ price:59,
 image:"./images/pacific-waters-box-cat.png",
 category:"soap",
  description:"A fresh, refined blend of rosemary, watermint and lavender, with warm notes of sandalwood, tobacco and amber. Made with olive oil, coconut oil and shea butter, and enriched with activated charcoal and seaweed powder to gently cleanse and refresh the skin."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -46,6 +51,7 @@ price:59,
 image:"./images/seashells-on-the-seaboard-box-cat.png",
 category:"soap",
  description:"A bright, coastal-inspired blend of lemongrass and sweet orange, grounded with cedarwood and patchouli for a fresh citrus scent with warm earthy notes. Inspired by Table Mountain and SA’s sparkling shoreline"
+ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g." 
 },
 
 {
@@ -54,6 +60,7 @@ price:59,
 image:"./images/just-beachy-box-cat.png",
 category:"soap",
  description:"A fresh seaside blend of neroli, lemon and eucalyptus, balanced with patchouli for a bright, uplifting ocean-inspired scent. Made with a nourishing blend of extra virgin olive oil, coconut oil, shea butter, castor oil, and responsibly sourced palm oil."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -62,6 +69,7 @@ price:59,
 image:"./images/la-vie-in-rose-box-cat.png",
 category:"soap",
  description:"A soft floral blend of rose, geranium and palmarosa creates a classic, elegant fragrance. Made with olive oil, coconut oil and shea butter. Fully encompassing the timeless scent of the Rose."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -70,6 +78,7 @@ price:59,
 image:"./images/protea-party-box-cat.png",
 category:"soap",
  description:"An uplifting blend of geranium, lemon and cedarwood creates a fresh, invigorating fragrance. Made with olive oil, coconut oil and shea butter, and enriched with poppy seeds for gentle exfoliation. Inspired by the delicate fragrance of the Protea."
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -77,6 +86,7 @@ name:"Boulders Cove Soap",
 price:59,
 image:"./images/boulders-cove-box-cat.png",
 category:"soap",
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
  description:"A fresh coastal blend of grapefruit and litsea cubeba, balanced with lavender, cedarwood, geranium and patchouli for a bright citrus scent with earthy undertones. Made with olive oil, coconut oil and shea butter."
 },
 
@@ -86,6 +96,7 @@ price:129,
 image:"./images/highveld-rose-salt-2.jpeg",
 category:"bath",
  description:"A calming floral bath soak infused with delicate rose fragrance and mineral-rich salts. Designed to relax the body while leaving skin feeling soft and refreshed."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Lavender Buds, Citrus Peel or Rooibos), Fragrance (Parfum), Cosmetic Colourants. Weight: 250g."
 },
 
 {
@@ -94,6 +105,7 @@ price:129,
 image:"./images/lavender-fynbos-salt-4.jpeg",
 category:"bath",
  description:"A soothing blend of lavender and herbal botanicals inspired by South Africa’s natural fynbos landscapes. Perfect for unwinding after a long day."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Lavender Buds, Citrus Peel or Rooibos), Fragrance (Parfum), Cosmetic Colourants. Weight: 250g."
 },
 
 {
@@ -102,6 +114,7 @@ price:129,
 image:"./images/coastal-citrus-salt-5.jpeg",
 category:"bath",
  description:"An uplifting fusion of citrus botanicals and premium bath salts, crafted to energise the senses while soothing tired muscles. This refreshing blend transforms your bath into a revitalising escape."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Lavender Buds, Citrus Peel or Rooibos), Fragrance (Parfum), Cosmetic Colourants. Weight: 250g."
 },
 
 {
@@ -110,6 +123,7 @@ price:129,
 image:"./images/rooibos-and-honey-salt-1.png",
 category:"bath",
  description:"A soothing blend of mineral-rich salts infused with calming rooibos and naturally comforting honey notes. Designed to relax the body, ease tension, and leave your skin feeling soft and restored. Perfect for unwinding after a long day."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Lavender Buds, Citrus Peel or Rooibos), Fragrance (Parfum), Cosmetic Colourants. Weight: 250g."
 },
 
 {
@@ -118,6 +132,7 @@ price:139,
 image:"./images/fynbos-tea-1.png",
 category:"bath",
  description:"Inspired by South Africa’s natural fynbos landscapes, this herbal bath tea combines aromatic botanicals to create a refreshing and grounding bathing experience. Weight: 100g Origin: Locally made."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Chamomile Flowers, Lavender Buds, Rooibos and Herbal Botanicals), Fragrance (Parfum). Weight: 100g."
 },
 
 {
@@ -126,6 +141,7 @@ price:139,
 image:"./images/rose-tea-1.png",
 category:"bath",
  description:"A soothing blend of rose petals and chamomile designed to gently infuse bath water with calming floral aromas. This botanical bath tea helps create a relaxing spa-like soak while softening the skin. Weight: 100g Origin: Locally made."
+ ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Chamomile Flowers, Lavender Buds, Rooibos and Herbal Botanicals), Fragrance (Parfum). Weight: 100g."
 },
 
 {
@@ -134,6 +150,7 @@ price:149,
 image:"./images/shower-steamers-1.jpg",
 category:"bath",
  description:"Transform your daily shower into a spa-like aromatherapy experience. These shower steamers release calming essential oil fragrances when activated by warm water and steam. Available Scents include Eucalyptus Refresh, Lavender Calm, Citrus Energy. Packaging: Boxed set Origin: Locally made."
+ ingredients: "Ingredients: Sodium Bicarbonate (Baking Soda), Citric Acid, Zea Mays (Cornstarch), Essential Oil Blends (including Eucalyptus, Lavender or Citrus Oils), Aqua (Water), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -142,6 +159,7 @@ price:329,
 image:"./images/bloom-and-breeze-set.png",
 category:"bundle",
  description:"A curated trio of mineral-rich botanical bath salts inspired by South African florals and coastal landscapes. Each blend is designed to transform an everyday bath into a relaxing self-care ritual. Includes the Highveld Rose Bath Salts, Lavender Fynbos Bath Salts and the Coastal Citrus Bath Salts. Weight: 250g per packet Origin: Locally made."
+  ingredients: "Ingredients: Magnesium Sulphate (Epsom Salt), Sodium Chloride (Sea Salt), Natural Botanicals (including Rose Petals, Lavender Buds, Citrus Peel or Rooibos), Fragrance (Parfum), Cosmetic Colourants. Weight: 250g."
 },
 
  {
@@ -150,6 +168,7 @@ price:159,
 image:"./images/floral-collection-cat.jpg",
 category:"bundle",
  description:"Including Fragrant Scents such as Jacaranda Bloom, Highveld Rose and Lily of the Vale. For a blooming moment of self-care"
+   ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -158,6 +177,7 @@ price:159,
 image:"./images/coastal-escape-cat.jpg",
 category:"bundle",
  description:"Including Coastal Scents such as Pacific Waters, Seashells on the Seaboard and Just Beachy. Fresh, clean, and kissed by the sea."
+  ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -166,6 +186,7 @@ price:179,
 image:"./images/birthday-bundle-cat.jpg",
 category:"bundle",
  description:"Celebrate life’s special moments with the Birthday Elegance Bundle. A beautifully crafted trio of botanical soaps designed to bring a touch of luxury to any birthday celebration. With elegant designs and uplifting fragrances, this bundle makes a thoughtful and memorable gift. Including Scents such as Golden Wish, Champagne Celebration and Confetti Glow"
+  ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 
 {
@@ -174,6 +195,7 @@ price:179,
 image:"./images/easter-bundle-cat.jpg",
 category:"bundle",
  description:"Welcome the freshness of spring with the Easter Joy Bundle, a cheerful trio of botanical soaps inspired by playful Easter traditions and blooming gardens. Perfect for seasonal gifting or adding a touch of festive charm to everyday self-care. Includes scents such as Bunny Meadow, Pastel Eggs and Hoppy Carrots"
+  ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
  {
  name:"Christmas Bundle",
@@ -181,6 +203,7 @@ price:179,
 image:"./images/christmas-bundle-cat.jpg",
 category:"bundle",
   description:"Celebrate the joy of the festive season with this limited-edition holiday soap trio. A festive trio of botanical soaps inspired by the magic of the holiday season. Perfect for gifting or adding a cheerful touch to your self-care routine. Including Scents such as Santa’s Suds, Frosted Pines and Evergreen Cheer"
+   ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
  
 {
@@ -189,6 +212,7 @@ price:289,
 image:"./images/evening-bloom-ritual-bundle.png",
 category:"bundle",
  description:"Designed to help you unwind after a long day, this ritual combines soothing botanicals and gentle fragrances to create a peaceful evening bath. Includes: Highveld Rose Botanical Soap, Highveld Rose Bath Salts, Rose & Chamomile Bath Tea"
+ ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants.Sodium Bicarbonate (Baking Soda), Citric Acid, Zea Mays (Cornstarch), Essential Oil Blends (including Eucalyptus, Lavender or Citrus Oils), Aqua (Water), Cosmetic Colourants. Weight: 100g."
 },
 
   {
@@ -197,6 +221,7 @@ price:399,
 image:"./images/botanical-spa-ritual.png",
 category:"bundle",
    description:"Inspired by traditional botanical spa treatments, this ritual blends calming floral fragrances with nourishing cleansing products to create a relaxing at-home spa experience. Includes: Jacaranda Bloom Botanical Soap, Lavender Fynbos Bath Salts, Fynbos Herbal Bath Tea and a Ceramic Soap Dish"
+   ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants.Sodium Bicarbonate (Baking Soda), Citric Acid, Zea Mays (Cornstarch), Essential Oil Blends (including Eucalyptus, Lavender or Citrus Oils), Aqua (Water), Cosmetic Colourants. Weight: 100g."
 },
 
   {
@@ -205,6 +230,7 @@ price:399,
 image:"./images/coastal-refresh-ritual.png",
 category:"bundle",
 description:"Fresh coastal fragrances combine with mineral bath salts to create a refreshing and energising bathing ritual inspired by South African shorelines. Includes: Pacific Waters Botanical Soap, Botanical Bath Salts, Cotton Soap Saver Pouch"
+   ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants.Sodium Bicarbonate (Baking Soda), Citric Acid, Zea Mays (Cornstarch), Essential Oil Blends (including Eucalyptus, Lavender or Citrus Oils), Aqua (Water), Cosmetic Colourants. Weight: 100g."
 },
 
   {
@@ -237,6 +263,7 @@ price:499,
 image:"./images/complete-botanical-self-care-ritual.jpg",
 category:"bundle",
    description:"Our signature self-care experience. The Botanical Ritual Set combines our most loved products into one indulgent collection, designed to relax the body, nourish the skin, and elevate your entire bathing ritual. The perfect gift or personal treat."
+   ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants.Sodium Bicarbonate (Baking Soda), Citric Acid, Zea Mays (Cornstarch), Essential Oil Blends (including Eucalyptus, Lavender or Citrus Oils), Aqua (Water), Cosmetic Colourants. Weight: 100g."
 },
 
   {
@@ -245,5 +272,6 @@ price:249,
 image:"./images/premium-collection-cat-1.jpg",
 category:"bundle",
    description:"A luxurious collection of handcrafted soaps infused with botanical extracts and nourishing oils. Each bar is carefully formulated to cleanse gently while leaving your skin soft, hydrated, and delicately scented. Designed to turn your daily routine into a moment of indulgence."
+    ingredients: "Ingredients: Olea Europaea (Olive) Oil, Cocos Nucifera (Coconut) Oil, Butyrospermum Parkii (Shea Butter), Aqua (Water), Sodium Hydroxide, Fragrance (Parfum), Cosmetic Colourants. Weight: 100g."
 },
 ];
