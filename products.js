72ade013f44ff864915cb783d9ac5ac67a3a6568
BeebzTrading <b.ztrading@outlook.com>
2026-03-17 09:38:38 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,36 +1,140 @@
-const mainImage = document.getElementById("main-product-image")
-const thumbnailContainer = document.getElementById("thumbnail-container")
+const products = [
 
-let allImages = []
+{
+  name: "Highveld Rose Soap",
+  price: 59,
+  image: "./images/highveld-rose-box-cat.png",
+  images: [
+    "./images/highveld-rose-1.jpg",
+    "./images/highveld-rose-2.jpg",
+    "./images/highveld-rose-3.jpg"
+  ],
+  category: "soap",
+  description: "A romantic floral soap with rose, geranium and palmarosa.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
 
-// Always add main image first
-if(product.image){
-  allImages.push(product.image)
+{
+  name: "Jacaranda Bloom Soap",
+  price: 59,
+  image: "./images/jacaranda-bloom-box-cat.png",
+  images: [
+    "./images/jacaranda-bloom-1.jpg",
+    "./images/jacaranda-bloom-2.jpg",
+    "./images/jacaranda-bloom-3.jpg"
+  ],
+  category: "soap",
+  description: "A calming floral blend inspired by jacaranda blossoms.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Lily of the Valley Soap",
+  price: 59,
+  image: "./images/lily-of-the-vale-box-cat.png",
+  images: [
+    "./images/lily-of-the-valley-1.jpg",
+    "./images/lily-of-the-valley-2.jpg",
+    "./images/lily-of-the-valley-3.jpg"
+  ],
+  category: "soap",
+  description: "A soft and elegant floral scent.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Port Lizzie Soap",
+  price: 59,
+  image: "./images/port-lizzie-box-cat.png",
+  images: [
+    "./images/port-lizzie-1.jpg",
+    "./images/port-lizzie-2.jpg"
+  ],
+  category: "soap",
+  description: "A fresh coastal-inspired blend.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Pacific Waters Soap",
+  price: 59,
+  image: "./images/pacific-waters-box-cat.png",
+  images: [
+    "./images/pacific-waters-1.jpg",
+    "./images/pacific-waters-2.jpg",
+    "./images/pacific-waters-3.jpg"
+  ],
+  category: "soap",
+  description: "A crisp, clean aquatic scent.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Seashells on the Seaboard Soap",
+  price: 59,
+  image: "./images/seashells-on-the-seaboard-box-cat.png",
+  images: [
+    "./images/seashells-seaboard-1.jpg",
+    "./images/seashells-seaboard-2.jpg"
+  ],
+  category: "soap",
+  description: "A bright seaside fragrance.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Just Beachy Soap",
+  price: 59,
+  image: "./images/just-beachy-box-cat.png",
+  images: [
+    "./images/just-beachy-1.jpg",
+    "./images/just-beachy-2.jpg"
+  ],
+  category: "soap",
+  description: "A playful beach-inspired scent.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "La Vie in Rose Soap",
+  price: 59,
+  image: "./images/la-vie-in-rose-box-cat.png",
+  images: [
+    "./images/la-vie-in-rose-1.jpg",
+    "./images/la-vie-in-rose-2.jpg",
+    "./images/la-vie-in-rose-3.jpg"
+  ],
+  category: "soap",
+  description: "A luxurious rose fragrance.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Protea Party Soap",
+  price: 59,
+  image: "./images/protea-party-box-cat.png",
+  images: [
+    "./images/protea-party-1.jpg",
+    "./images/protea-party-2.jpg",
+    "./images/protea-party-3.jpg"
+  ],
+  category: "soap",
+  description: "An uplifting floral blend inspired by proteas.",
+  ingredients: "Olive oil, coconut oil, shea butter"
+},
+
+{
+  name: "Boulders Cove Soap",
+  price: 59,
+  image: "./images/boulders-cove-box-cat.png",
+  images: [
+    "./images/boulders-cove-1.jpg",
+    "./images/boulders-cove-2.jpg",
+    "./images/boulders-cove-3.jpg"
+  ],
+  category: "soap",
+  description: "A fresh coastal scent with mineral notes.",
+  ingredients: "Olive oil, coconut oil, shea butter"
 }
 
-// Then add gallery images safely
-if(product.images && Array.isArray(product.images)){
-  for(let i = 0; i < product.images.length; i++){
-    allImages.push(product.images[i])
-  }
-}
-
-// Create thumbnails
-for(let i = 0; i < allImages.length; i++){
-
-  const img = allImages[i]
-
-  const thumb = document.createElement("img")
-  thumb.src = img
-  thumb.style.width = "70px"
-  thumb.style.height = "70px"
-  thumb.style.objectFit = "cover"
-  thumb.style.cursor = "pointer"
-  thumb.style.borderRadius = "6px"
-
-  thumb.onclick = function(){
-    mainImage.src = img
-  }
-
-  thumbnailContainer.appendChild(thumb)
-}
+];
